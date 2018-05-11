3758ac5e90893801b9bcb65d4b8755d1e68ec960
EC2 Default User <ec2-user@ip-172-31-12-76.us-east-2.compute.internal>
2018-05-11 07:52:55 +0000

    20180511 heroku test5b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_filter :ensure_domain
+  FQDN = 'www.koluku.net'
+  # redirect correct server from herokuapp domain for SEO
+  def ensure_domain
+   return unless /\.herokuapp.com/ =~ request.host
+  
+   # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める 
+   port = ":#{request.port}" unless [80, 443].include?(request.port)
+   redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
+  end
+
+
+
   include SessionsHelper
   
   private
