84a6623c4ef14c7b059cf9f83f51c2389d667c05
aqua-style <takuma@happiant.co.jp>
2019-07-18 15:05:18 +0900

    stop redirect to www.koluku.net

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-
-  #herokuapp.comから独自ドメインへリダイレクト
-  before_filter :ensure_domain
-  FQDN = 'www.koluku.net'
-  # redirect correct server from herokuapp domain for SEO
-  def ensure_domain
-   return unless /\.herokuapp.com/ =~ request.host
-  
-   # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める 
-   port = ":#{request.port}" unless [80, 443].include?(request.port)
-   redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
-  end
-
+  #20190718 独自ドメインwww.koluku.netへ飛ばすのを止める
 
   include SessionsHelper
   
